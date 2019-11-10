e1f1e8ddff206836ec45144a12e29dfbb095d263
MatiasRei <matiasreimondo@gmail.com>
2019-11-10 14:04:42 -0300

    Reestructuracion del controlle ping all funcionanando

--- controller/controller.py
+++ controller/controller.py
@@ -6,6 +6,8 @@ from pox.lib.util import dpid_to_str
 
 from extensions.graph import Graph
 from extensions.switch import SwitchController
+import pox.host_tracker
+
 
 log = core.getLogger()
 
@@ -13,8 +15,10 @@ log = core.getLogger()
 class Controller:
     def __init__(self):
         self.connections = set()
-        self.switches = Graph()  # "Grafo" de adyacencias
-        self.links = {}  # Mapeo de dpids a direcciones ethernet
+        #self.switches = Graph()  # "Grafo" de adyacencias
+        self.switches = {}
+        self.paths = {}
+        #self.links = {}  # Mapeo de dpids a direcciones ethernet
         # Esperando que los modulos openflow y openflow_discovery esten listos
         core.call_when_ready(self.startup, ('openflow', 'openflow_discovery'))
 
@@ -34,20 +38,124 @@ class Controller:
         Se encarga de crear un nuevo switch controller para manejar los eventos de cada switch
         """
         log.info("Switch %s has come up.", dpid_to_str(event.dpid))
-        self.links[event.dpid] = event.connection.eth_addr
         if (event.connection not in self.connections):
             self.connections.add(event.connection)
-            SwitchController(event.dpid, event.connection, self.switches)
+            switch = SwitchController(event.dpid, event.connection, self)
+
+            #Lo agrego al diccionario de switches
+            self.switches[event.dpid] = switch
 
     def _handle_LinkEvent(self, event):
         """
         Esta funcion es llamada cada vez que openflow_discovery descubre un nuevo enlace
         """
         link = event.link
-        ### posiblemente haya que mapear los ethernet internos del switch, todas los puertos tienen un eth tambien
-        log.info("Link has been discovered from %s,%s to %s,%s", dpid_to_str(link.dpid1), link.port1,
-                 dpid_to_str(link.dpid2), link.port2)
-        self.switches.add_link(self.links[link.dpid1], link.port1, self.links[link.dpid2], link.port2)
+        #log.info("Link has been discovered from %s,%s to %s,%s", dpid_to_str(link.dpid1), link.port1,  dpid_to_str(link.dpid2), link.port2)
+        #self.switches.add_link(self.links[link.dpid1], link.port1, self.links[link.dpid2], link.port2)
+        self.switches[link.dpid1].add_link_port(link.dpid2,link.port1)
+        self.switches[link.dpid2].add_link_port(link.dpid1,link.port2)
+
+
+    def assign_route(self, switch_id, packet, port_in, data):
+        start = switch_id
+        end = packet.dst
+
+        not_visitted = []
+        acum_dist = {}
+        precesors = {}
+        possible_last_switches = []
+
+        for switch in self.switches.keys():
+            #Agrego todos los switch a los no visitados
+            not_visitted.append(switch)
+            #La distancia a cada switch es infinita
+            acum_dist[switch] = 99999
+
+            #Recorro cada host del switch si alguno de los switch
+            #Tiene como host el destino del paquete
+            #Lo agrego a la lista de switch final
+            for port, mac in self.switches[switch].hosts_adyascents():
+                if (end == mac):
+                    possible_last_switches.append([switch, port])
+
+        #Se desconoce como llegar al host
+        if len(possible_last_switches) == 0:
+            return
+
+        #Distancia acumulada desde el comienzo
+        acum_dist[start] = 0
+        #El ultimo visitado
+        last = start
+
+        while (len(not_visitted) > 0):
+            not_visitted.remove(last)
+
+            for port, adyascent in self.switches[last].ports_adyascents():
+                #Los adyascentes estan a una unidad de distancia
+                if (acum_dist[adyascent] > acum_dist[last] + 1):
+                    acum_dist[adyascent] = acum_dist[last] + 1
+                    #Append Switch and the port that goes to this switch
+                    precesors[adyascent] = [last, port]
+
+
+            minor_dist = 99999
+
+            for switch in self.switches.keys():
+                if (switch not in not_visitted):
+                    continue
+
+                if (acum_dist[switch] < minor_dist):
+                    last = switch
+                    minor_dist = acum_dist[switch]
+
+        # Get shortest path
+        path = []
+        actual_dist = 99999
+        last_switch = 0
+        port = 0
+
+        #Por ahora solo tomamos un unico switch adyascente al host destino
+        #Esto es valido siempre que se respete fat tree
+        #Donde cada datacenter se conecta con un unico switch
+        #Por lo que la lista deberia ser de 1 solo elemento
+
+        for switch, p in possible_last_switches:
+            if (acum_dist[switch] < actual_dist):
+                last_switch = switch
+                port = p
+                actual_dist = acum_dist[switch]
+
+        #Esta conectado directamente con el switch
+        if (last_switch == start):
+            path.append([port_in, last_switch, port])
+        else:
+            while (last_switch in precesors):
+                sw, p = precesors[last_switch]
+
+                for p2, sw2 in self.switches[last_switch].ports_adyascents():
+                    if sw2 == sw:
+                        path.append([p2, last_switch, port])
+                        port = p
+                        break
+                last_switch = sw
+
+            path.append([port_in, last_switch, port])
+
+
+        for in_port, switch, exit_port in reversed(path):
+            self.switches[switch].add_route(
+                in_port,
+                exit_port,
+                packet.src,
+                packet.dst,
+                packet.type,
+                packet.payload.srcip,
+                packet.payload.dstip,
+                packet.payload.protocol
+            )
+
+
+
 
 def launch():
     # Inicializando el modulo openflow_discovery
--- controller/extensions/switch.py
+++ controller/extensions/switch.py
@@ -6,42 +6,91 @@ log = core.getLogger()
 
 
 class SwitchController:
-    def __init__(self, dpid, connection, switches):
+    def __init__(self, dpid, connection, controller):
         self.dpid = dpid
         self.connection = connection
         # El SwitchController se agrega como handler de los eventos del switch
         self.connection.addListeners(self)
-        self.switches = switches
+        self.controller = controller
+        self.hosts = {}
+        self.ports = {}
+
+        self.cost = 0
+        self.routes = []
+
+
+    def add_link_port(self, switch_id, port):
+        self.ports[port] = switch_id
+
+    def add_host(self, macaddr, port):
+        self.host[port] = macaddr
+
+
+    def ports_adyascents(self):
+        return self.ports.items()
+
+    def hosts_adyascents(self):
+        return self.hosts.items()
+
+    def add_route(self, in_port, exit_port, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_type):
+        self.routes.append([in_port, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_type, exit_port])
+        # Aumentamos el costo de pasar por este switch en 1 para controlar
+        # el trafico
+        self.cost += 1
+
+    def search_route(self,event,packet):
+
+        for in_port, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_type, exit_port in self.routes:
+            if (event.port == in_port and
+                eth_src == packet.src and
+                eth_dst == packet.dst and
+                eth_type == packet.type and
+                ip_src == packet.payload.srcip and
+                ip_dst == packet.payload.dstip and
+                ip_type == packet.payload.protocol):
+
+                self.route_msg(in_port, exit_port, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_type, event.ofp)
+                return True
+
+        #No se encontro la ruta en las rutas existentes
+        return False
+
+    def route_msg(self, in_port, exit_port, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_type, data):
+        msg = of.ofp_flow_mod()
+        msg.data = data
+        msg.command = of.OFPFC_ADD
+        msg.match.dl_dst = eth_dst
+        msg.match.dl_src = eth_src
+        msg.match.in_port = in_port
+        msg.match.dl_type = eth_type
+        msg.match.nw_src = ip_src
+        msg.match.nw_dst = ip_dst
+        msg.match.nw_proto = ip_type
+
+        msg.actions.append(of.ofp_action_output(port = exit_port))
+        log.info("Sending to switch: %s from %s to %s port in: %s out: %s.", self.dpid, eth_src, eth_dst, in_port, exit_port)
+        self.connection.send(msg)
+
+
 
     def _handle_PacketIn(self, event):
         """
         Esta funcion es llamada cada vez que el switch recibe un paquete
         y no encuentra en su tabla una regla para rutearlo
         """
-
         packet = event.parsed
-        path = self.switches.resolve_path(self.connection.eth_addr, packet)
-        log.info("Path encontrado {}".format(path))
-        if not path:
-            return
-        # SRC IP = packet.payload.srcip
-        # DST IP = packet.payload.dstip
-        # SRC PORT = packet.payload.payload.srcport
-        # DST PORT = packet.payload.payload.dstport
 
-        import pdb; pdb.set_trace()
+        #Si un paquete arribo y no esta en la lista de puertos quiere decir
+        #Que es un host perteneciente al switch
+        if (event.port not in self.ports.keys()):
+            self.hosts[event.port] = packet.src
+
         if packet.type != packet.IP_TYPE:  # Solo manejamos IPv4
             return
 
-        if event.port == 1:
-            out_port = 2
-        else:
-            out_port = 1
+        ruteado = self.search_route(event, packet)
+        if not ruteado:
+            # Obtenemos y asignamos una ruta hacia el destino
+            self.controller.assign_route(self.dpid, packet, event.port, event.ofp)
+            self.search_route(event,packet)
 
-        msg = of.ofp_flow_mod()
-        msg.data = event.ofp
-        msg.priority = of.OFP_DEFAULT_PRIORITY
-        msg.match.dl_type = 0x800
-        msg.match.nw_dst = packet.payload.dstip
-        msg.actions.append(of.ofp_action_output(port=out_port))
-        self.connection.send(msg)
